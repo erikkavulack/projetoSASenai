353368ccf3f9894f0cd65eecb280357b84e6e0c9
erikkavulack <kavulacke@gmail.com>
2024-12-05 22:13:42 -0300

    05/12

--- cadastro/script.js
+++ cadastro/script.js
@@ -1,39 +1,36 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const form = document.getElementById('cadastroForm');
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
-        const placa = document.getElementById('placa').value;
-        const marca = document.getElementById('marca').value;
-        const modelo = document.getElementById('modelo').value;
-        const ano = document.getElementById('ano').value;
+const campoNovoNome = document.getElementById("campoNovoNome");
+const campoNovoEmail = document.getElementById("campoNovoEmail");
+const campoRepSenha = document.getElementById("campoRepSenha");
 
-        let veiculos = JSON.parse(localStorage.getItem('garagem')) || [];
-        let id = veiculos.length ? veiculos[veiculos.length - 1].id + 1 : 1;
-        while (veiculos.some(veiculo => veiculo.id === id)) {
-    		id++;
+function  cadastra(){
+    if (campoNovaSenha.value == campoRepSenha.value) {
+        const usuario = {
+            nome: campoNovoNome.value,
+            login: campoNovoEmail.value,
+            senha: campoNovaSenha.value
+        };
+        let bancoDeDados = JSON.parse(localStorage.getItem("bancoDeDados"));
+        if (bancoDeDados == null) {
+            bancoDeDados = [];
         }
-
-        const veiculo = { id, placa, marca, modelo, ano };
-
-        veiculos.push(veiculo);
-        localStorage.setItem('garagem', JSON.stringify(veiculos));
+        bancoDeDados.push(usuario);
+        localStorage.setItem("bancoDeDados", JSON.stringify(bancoDeDados));
         modal.style.display = "block";
-        form.reset();
-    });
-});
+        window.location.href = "/bibliotecas/index.html"
+    } else {
+        alert("As senhas são diferentes!");
+    }
+    
+}
 
-// Obtém o modal
 let modal = document.getElementById("myModal");
 
-// Obtém o elemento <span> que fecha o modal
 let span = document.getElementsByClassName("close")[0];
 
-// Quando o usuário clicar no <span> (x), fecha o modal
 span.onclick = function() {
     modal.style.display = "none";
 }
 
-// Quando o usuário clicar em qualquer lugar fora do modal-content, fecha o modal
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
---
+++ login/script.js
@@ -0,0 +1,25 @@
+const campoLogin = document.getElementById("login");
+const campoSenha = document.getElementById("password");
+const campoNovoLogin = document.getElementById("novoLogin");
+const campoNovaSenha = document.getElementById("novaSenha");
+const campoRepSenha = document.getElementById("repSenha");
+
+function login(){
+    let login = campoLogin.value;
+    let senha = campoSenha.value;
+    let mensagem = "Usuário ou senha incorreta!";
+    let bancoDeDados = JSON.parse(localStorage.getItem("bancoDeDados"));
+    if (bancoDeDados == null) {
+        mensagem = "Nenhum usuário cadastrado até o momento";
+    } else {
+        for (let usuario of bancoDeDados) {
+            if (usuario.login == login && usuario.senha == senha) {
+                mensagem = "Parabéns, você logou!";
+                localStorage.setItem("logado", JSON.stringify(usuario));
+                window.location.href = "home.html";
+                break;
+            }
+        }        
+    }
+    alert(mensagem);
+}
